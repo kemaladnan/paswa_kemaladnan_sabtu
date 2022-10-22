68d12ff46bc258d7210be910ae702d6d426a1489
Kemal Adnan <adnkml17@gmail.com>
2022-10-22 17:56:49 +0700

    tes

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'db_paswa_sabtu';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect(
+    $databaseHost,
+    $databaseUsername,
+    $databasePassword,
+    $databaseName
+);
+?>
